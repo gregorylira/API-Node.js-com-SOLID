de7d2682674fd117c33edc7ee46d2eb555a05814
gregorylira <gregory.lira007@gmail.com>
2024-05-25 08:20:29 -0300

    configure eslint, aliases imports and envs

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
-import { app } from "./app";
+import { app } from './app'
+import { env } from './env'
 
-app.listen({
+app
+  .listen({
     host: '0.0.0.0',
-    port: 3333
-}).then(()=>{
-    console.log('HTTP Server Running');
-})
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('HTTP Server Running')
+  })

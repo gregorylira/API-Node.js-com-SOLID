1d8e0a55ee109db17d0a9607aab2057df6d93ae5
gregorylira <gregory.lira007@gmail.com>
2024-05-29 12:05:26 -0300

    add new middleware to autenticate

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -2,11 +2,12 @@ import { FastifyInstance } from 'fastify'
 import { register } from './controllers/register'
 import { authenticate } from './controllers/authenticate'
 import { profile } from './controllers/profile'
+import { verifyJwt } from './middlewares/verify-jwt'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/user', register)
   app.post('/session', authenticate)
 
   /** Authenticated */
-  app.get('/me', profile)
+  app.get('/me', { onRequest: verifyJwt }, profile)
 }

027802d42ae891f88450b5a123ad101d8f731aa5
gregorylira <gregory.lira007@gmail.com>
2024-05-29 11:06:01 -0300

    config: add a jwt to autenticate routes

--- src/env/index.ts
+++ src/env/index.ts
@@ -4,6 +4,7 @@ import z from 'zod'
 const envSchema = z.object({
   NODE_ENV: z.enum(['dev', 'production', 'test']).default('dev'),
   PORT: z.coerce.number().default(3000),
+  JWT_SECRET: z.string(),
 })
 
 const _env = envSchema.safeParse(process.env)
--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -15,13 +15,23 @@ export async function authenticate(
   const { email, password } = authenticateBodySchema.parse(request.body)
   try {
     const authenticateUseCase = makeAuthenticateUseCase()
-    await authenticateUseCase.execute({ email, password })
+    const { user } = await authenticateUseCase.execute({ email, password })
+
+    const token = await reply.jwtSign(
+      {},
+      {
+        sign: {
+          sub: user.id,
+        },
+      },
+    )
+    return reply.status(200).send({
+      token,
+    })
   } catch (err) {
     if (err instanceof InvalidCredentialsError) {
       return reply.status(400).send({ message: err.message })
     }
     throw err
   }
-
-  return reply.status(200).send()
 }
